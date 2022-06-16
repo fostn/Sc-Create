34bef0cbcc506386f25b8b8fb4a9d1b34d190616
fostn <106077105+fostn@users.noreply.github.com>
2022-06-17 00:39:04 +0300

    Add files via upload

---
+++ Sc-Create.py
@@ -0,0 +1,33 @@
+import requests
+print("""
+
+
+█▀ █▀▀   █▀▀ █▀█ █▀▀ ▄▀█ ▀█▀ █▀▀
+▄█ █▄▄   █▄▄ █▀▄ ██▄ █▀█ ░█░ ██▄
+
+[Create Snapchat Account]
+_________________________
+
+By Fostn
+Insta @f09l
+Twitter @lwv5
+Telegram Channel @ifostn
+_________________________
+""")
+user = input("Enter Username : ")
+email = input("Enter Email : ")
+password = input("Enter Password : ")
+url = (f"https://tufaah.osc-fr1.scalingo.io/snapchat/?user={user}&email={email}&password={password}")
+r = requests.get(url).json()
+if r['error'] == 'Taken user':
+	print('\n''Username Taken')
+elif r['error'] == 'Something went wrong':
+	print('Try another Email ')
+else:
+	print('[successfully created]')
+	print('_'*21)
+	print("Username : "+user)
+	print('Email : '+email)
+	print('Password : '+password)
+	print('_'*21)
+	print('[For More Tools Fowllow Me]')
